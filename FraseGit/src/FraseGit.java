9619f7e8436b98ea1944ca076f5c2d3256357426
msanchezperis <fresi_con_nata@hotmail.com>
2013-10-28 18:33:21 +0100

    Aquí hi ha comentari amb conflicte

--- FraseGit/src/FraseGit.java
+++ FraseGit/src/FraseGit.java
@@ -2,7 +2,7 @@
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
-
+//això portarà conflicte
 public class FraseGit {
 
 	public static void main(String[] args) {
